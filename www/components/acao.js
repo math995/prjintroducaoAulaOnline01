d0f392518aca3909d181a01f88c97c48e1a933f9
math995 <matheusgamerbr443@gmail.com>
2021-03-10 05:22:14 +0900

    Acrescentei o JS para a calculadora funcionar, criei o input das operações e por último adicionei um background-color

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,49 @@
 // This is a JavaScript file
+$(document).on("click","#somar",function(){
+  var valor1 = $("#valor1").val();
+  var valor2 = $("#valor2").val();
 
+  var resultado = parseFloat(valor1) + parseFloat(valor2);
+
+  $("#resultado").val(resultado);
+});
+$(document).on("click","#subtracao",function(){
+  var valor1 = $("#valor1").val();
+  var valor2 = $("#valor2").val();
+
+  var resultado = parseFloat(valor1) - parseFloat(valor2);
+
+  $("#resultado").val(resultado);
+});
+$(document).on("click","#multiplicacao",function(){
+  var valor1 = $("#valor1").val();
+  var valor2 = $("#valor2").val();
+
+  var resultado = parseFloat(valor1) * parseFloat(valor2);
+
+  $("#resultado").val(resultado);
+});
+$(document).on("click","#divisao",function(){
+  var valor1 = $("#valor1").val();
+  var valor2 = $("#valor2").val();
+
+  var resultado = parseFloat(valor1) / parseFloat(valor2);
+
+  $("#resultado").val(resultado);
+});
+$(document).on("click","#exponenciacao",function(){
+  var valor1 = $("#valor1").val();
+  var valor2 = $("#valor2").val();
+
+  var resultado = parseFloat(valor1) / parseFloat(valor2);
+
+  $("#resultado").val(resultado);
+});
+$(document).on("click","#exponenciacao",function(){
+  var valor1 = $("#valor1").val();
+  var valor2 = $("#valor2").val();
+
+  var resultado = parseFloat(Math.pow(valor1,valor2));
+
+  $("#resultado").val(resultado);
+});
